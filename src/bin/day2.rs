b05f35ef3d62abc05ef9851fecf703ff931d3224
Christopher Vittal <christopher.vittal@gmail.com>
2017-12-02 01:55:34 -0500

    Solve day 2, also add library function

---
+++ src/bin/day2.rs
@@ -0,0 +1,34 @@
+
+extern crate aoc;
+
+use aoc::file;
+
+fn main() {
+    let s = file::to_split_parsed("data/day2");
+    println!("1: {}", checksum(&s));
+    println!("2: {}", checksum2(&s));
+}
+
+fn checksum(data: &Vec<Vec<u32>>) -> u32 {
+    let mut tot = 0;
+    for v in data {
+        tot += v.iter().max().unwrap() - v.iter().min().unwrap()
+    }
+    tot
+}
+
+fn checksum2(data: &Vec<Vec<u32>>) -> u32 {
+    let mut tot = 0;
+    for v in data {
+        for (i, &d) in v.iter().enumerate() {
+            for &d2 in v[i+1..].iter() {
+               if d > d2 && d % d2 == 0 {
+                   tot += d / d2;
+               } else if d2 % d == 0 {
+                   tot += d2 / d;
+               }
+            }
+        }
+    }
+    tot
+}
--- src/file.rs
+++ src/file.rs
@@ -3,6 +3,7 @@ use std::io::prelude::*;
 use std::io;
 use std::fs::File;
 use std::path::Path;
+use std::str::FromStr;
 
 /// Simple function that collects a file into a `Lines` iterator, panics on
 /// any error.
@@ -16,4 +17,20 @@ pub fn to_lines<P: AsRef<Path>>(p: P) -> io::Lines<io::BufReader<File>> {
 /// any error.
 pub fn to_strings<P: AsRef<Path>>(p: P) -> Vec<String> {
     to_lines(p).map(|r| r.expect("error in read")).collect()
+}
+
+/// Converts input into parsed vector
+pub fn to_split_parsed<F, P>(p: P) -> Vec<Vec<F>>
+where
+    P: AsRef<Path>,
+    F: FromStr
+{
+    to_lines(p).map(|r| r.expect("error in read"))
+        .map(|s| s.split_whitespace()
+             .map(|v| match v.parse() {
+                 Err(_) => panic!("parse error for input: {}", v),
+                 Ok(vp) => vp
+             })
+             .collect())
+        .collect()
 }
