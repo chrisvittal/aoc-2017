4a818967310c62f6d023f9e39a672a993fa62fd5
Christopher Vittal <christopher.vittal@gmail.com>
2017-12-12 20:37:38 -0500

    inline update functions for day 5

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -3,16 +3,28 @@ extern crate aoc;
 
 fn main() {
     let input: Vec<i32> = aoc::file::to_single_parsed("data/day05");
-    println!("1: {}", skip(input.clone(), |_| 1));
-    println!("2: {}", skip(input, |t| if t >= 3 { -1 } else { 1 }));
+    println!("1: {}", skip1(input.clone()));
+    println!("2: {}", skip2(input));
 }
 
-fn skip(mut lst: Vec<i32>, f: fn(i32) -> i32) -> u32 {
+fn skip1(mut lst: Vec<i32>) -> u32 {
     let mut ret = 0;
     let mut ind = 0;
     while ind < lst.len() as i32 && ind >= 0 {
         let tmp = lst[ind as usize];
-        lst[ind as usize] += f(tmp);
+        lst[ind as usize] += 1;
+        ind += tmp;
+        ret += 1;
+    }
+    ret
+}
+
+fn skip2(mut lst: Vec<i32>) -> u32 {
+    let mut ret = 0;
+    let mut ind = 0;
+    while ind < lst.len() as i32 && ind >= 0 {
+        let tmp = lst[ind as usize];
+        lst[ind as usize] += if tmp >= 3 { -1 } else { 1 };
         ind += tmp;
         ret += 1;
     }
