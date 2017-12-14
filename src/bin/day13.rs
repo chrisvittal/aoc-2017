0242bf3fdf969b8fd9d55e4de2c6a788d105777b
Christopher Vittal <christopher.vittal@gmail.com>
2017-12-13 21:52:07 -0500

    add sort to improve loop efficiency
    also clean up iterator to use find. This saves around 10 ms.

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -4,15 +4,16 @@ extern crate aoc;
 const INPUT: &'static str = "data/day13";
 
 fn main() {
-    let input: Vec<(usize,usize)> = aoc::file::to_lines(INPUT).map(|l| {
+    let mut input: Vec<(usize,usize)> = aoc::file::to_lines(INPUT).map(|l| {
         let tmp = l.unwrap();
         let mut it = tmp.split(": ");
         let x: usize = it.next().unwrap().parse().unwrap();
         let y: usize = it.next().unwrap().parse().unwrap();
         (x, 2*(y-1))
     }).collect();
+    input.sort_unstable_by_key(|&(x,y)| (y,x));
     let one = input.iter().fold(0, |a, &(d,r)| if d%r == 0 { a + d*(r+2)/2 } else { a });
     println!("1: {}", one);
-    let two = (0..).filter(|s| !input.iter().any(|&(d, r)| (d+s) % r == 0)).next().unwrap();
+    let two = (0..).find(|s| !input.iter().any(|&(d, r)| (d+s) % r == 0)).unwrap();
     println!("2: {}", two);
 }
