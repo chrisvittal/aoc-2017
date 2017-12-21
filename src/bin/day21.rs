df12eea4fb3a0dc97a475ecf8e75336fa3340e13
Christopher Vittal <christopher.vittal@gmail.com>
2017-12-21 14:53:09 -0500

    wrap day 21 logic in struct and iterator

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -14,6 +14,11 @@ const INPUT: &'static str = "data/day21";
 
 type GridMap = HashMap<Matrix<bool>, (usize, Matrix<bool>)>;
 
+struct Grid<'a> {
+    grid: Matrix<bool>,
+    map: &'a GridMap
+}
+
 fn extend_map(map: &mut GridMap, s: &str) {
     let (from, to) = s.split(" => ")
         .map(|s| Matrix::square_from_vec(s.bytes().filter_map(|b| match b {
@@ -27,39 +32,46 @@ fn extend_map(map: &mut GridMap, s: &str) {
     map.extend(it);
 }
 
+#[inline]
 fn count(m: &Matrix<bool>) -> usize {
     m.as_ref().iter().filter(|&b| *b).count()
 }
 
-fn run(grid: &mut Matrix<bool>, map: &GridMap) -> usize {
-    let mut count = 0;
-    let pt_siz = 2 + grid.rows % 2;
-    let sl_cnt = grid.rows / pt_siz;
-    let mut new_grid = Matrix::new_square(sl_cnt + grid.rows, false);
-    for (sq_j, sq_i) in iproduct!(0..sl_cnt, 0..sl_cnt) {
-        let (c, ref new) = map[&grid.slice(sq_i*pt_siz..sq_i*pt_siz+pt_siz,
-                                      sq_j*pt_siz..sq_j*pt_siz+pt_siz)];
-        new_grid.set_slice(&(sq_i*(pt_siz+1), sq_j*(pt_siz+1)), new);
-        count += c;
+impl<'a> Grid<'a> {
+    #[inline]
+    fn run(&mut self) -> usize {
+        let mut count = 0;
+        let pt_siz = 2 + self.grid.rows % 2;
+        let sl_cnt = self.grid.rows / pt_siz;
+        let mut new_grid = Matrix::new_square(sl_cnt + self.grid.rows, false);
+        for (sq_j, sq_i) in iproduct!(0..sl_cnt, 0..sl_cnt) {
+            let (c, ref new) =
+                self.map[&self.grid.slice(sq_i*pt_siz..sq_i*pt_siz+pt_siz,
+                                          sq_j*pt_siz..sq_j*pt_siz+pt_siz)];
+            new_grid.set_slice(&(sq_i*(pt_siz+1), sq_j*(pt_siz+1)), new);
+            count += c;
+        }
+        self.grid = new_grid;
+        count
     }
-    *grid = new_grid;
-    count
+}
+
+impl<'a> Iterator for Grid<'a> {
+    type Item = usize;
+    fn next(&mut self) -> Option<usize> {
+        Some(self.run())
+    }
 }
 
 fn main() {
     const ONE: usize = 5;
     const TWO: usize = 18;
-    let start = vec![false,  true, false, false, false,  true, true,  true,  true]; 
-    let mut grid = Matrix::square_from_vec(start);
     let mut map = HashMap::new();
     aoc::file::to_lines(INPUT).for_each(|s| extend_map(&mut map, &s.unwrap()));
-    let mut count = 0;
-    for _ in 0..ONE {
-        count = run(&mut grid, &map);
-    }
-    println!("1: {}", count);
-    for _ in ONE..TWO {
-        count = run(&mut grid, &map);
-    }
-    println!("2: {}", count);
+
+    let start = vec![false,  true, false, false, false,  true, true,  true,  true];
+    let grid = Matrix::square_from_vec(start);
+    let mut grid = Grid { grid: grid, map: &map };
+    println!("1: {}", grid.nth(ONE-1).unwrap());
+    println!("2: {}", grid.nth(TWO-ONE-1).unwrap());
 }
